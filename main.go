b4c9e1a37154ddcdb0a38624058ad986c72e1817
Jumbef <jb.faucheron@gmail.com>
2021-03-07 01:32:19 +0100

    Add Buffer reset when all data read.

--- main.go
+++ main.go
@@ -13,57 +13,155 @@ package main
 */
 import (
 	"bufio"
+	"flag"
+	"io"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"dasa.cc/snd"
 	"dasa.cc/snd/al"
 )
 
+// SourceReader ...
+type SourceReader struct {
+	fromstring bool
+	stringData string
+	filePtr    *os.File
+	myReader   io.Reader
+	myBuffer   *bufio.Reader
+}
+
+// InitFromFile ...
+func (r *SourceReader) InitFromFile(s string) {
+	var err error
+	r.filePtr, err = os.Open(s)
+	if err != nil {
+		log.Println("File unopenable.")
+		log.Fatal(err)
+	}
+	r.fromstring = false
+	r.Reset()
+}
+
+// InitFromString ...
+func (r *SourceReader) InitFromString(s string) {
+	r.fromstring = true
+	r.stringData = s
+	r.Reset()
+}
+
+// Reset ...
+func (r *SourceReader) Reset() {
+	if r.fromstring {
+		r.myReader = strings.NewReader(r.stringData)
+		r.myBuffer = bufio.NewReader(r.myReader)
+	} else {
+		r.myReader = io.Reader(r.filePtr)
+		r.myBuffer = bufio.NewReader(r.myReader)
+	}
+	var err error
+	_, err = r.myBuffer.ReadByte()
+	if err != nil {
+		log.Println("Data unreadble.")
+		log.Fatal(err)
+	}
+	err = r.myBuffer.UnreadByte()
+	if err != nil {
+		log.Println("Data unreadble.")
+		log.Fatal(err)
+	}
+}
+
+// CloseFile ...
+func (r *SourceReader) CloseFile() {
+	if r.fromstring {
+		r.filePtr.Close()
+	}
+}
+
 func main() {
+	// Args parse
+	var filePath string
+	flag.StringVar(&filePath, "f", "", "A file path")
 
-	ex, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
+	var stringData string
+	flag.StringVar(&stringData, "s", "", "A string")
+
+	flag.Parse()
+
+	// vars
+	var datas [2]byte
+	var source SourceReader
+	var err error
+
+	// data buffer init
+	if stringData == "" {
+		if filePath == "" {
+			filePath, err = os.Executable()
+			if err != nil {
+				log.Println("Executable path unknow.")
+				log.Fatal(err)
+			}
+		}
+		log.Print(filePath)
+		source.InitFromFile(filePath)
+	} else {
+		source.InitFromString(stringData)
 	}
-	log.Print(ex)
 
-	f, err := os.Open(ex)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	reader := bufio.NewReader(f)
-
+	// Sound init
 	master := snd.NewMixer()
 	const buffers = 1
 	if al.OpenDevice(buffers); err != nil {
 		log.Fatal(err)
 	}
+	defer al.CloseDevice()
 	al.Start(master)
+	defer al.Stop()
 
 	sine := snd.Sine()
+	square := snd.Square()
 	mod := snd.NewOscil(sine, 0, nil)
-	osc := snd.NewOscil(sine, 440, mod) // oscillator
+	osc := snd.NewOscil(square, 440, mod) // oscillator
 
 	master.Append(osc)
 	al.Notify()
 
-	freq := float64(1)
-	data := byte(0)
+	// main loop
+	var freqs [2]float64
+
 	for range time.Tick(time.Second) {
-		data = 0
-		for data == 0 {
-			data, err = reader.ReadByte()
+		datas[0] = 0
+		for datas[0] == 0 {
+			if source.myBuffer.Buffered() == 0 {
+				source.Reset()
+				log.Println("Buffer reseted")
+			}
+			datas[0], err = source.myBuffer.ReadByte()
 			if err != nil {
+				log.Println("Source data unreadable.")
 				log.Fatal(err)
 			}
 		}
-		freq = 0.5 + float64(int(data))/64
-		log.Printf("> underruns=%-4v buflen=%-4v tickavg=%-12s drift=%-12s | byte=%-4v freq=%v\n",
-			al.Underruns(), al.BufLen(), al.TickAverge(), al.DriftApprox(), data, freq)
-		mod.SetFreq(freq, nil)
+		datas[1] = 0
+		for datas[1] == 0 {
+			if source.myBuffer.Buffered() == 0 {
+				source.Reset()
+				log.Println("Buffer reseted")
+			}
+			datas[1], err = source.myBuffer.ReadByte()
+			if err != nil {
+				log.Println("Source data unreadable.")
+				log.Fatal(err)
+			}
+		}
+		freqs[0] = 0.5 + float64(int(datas[0]))/64
+		freqs[1] = 100 * (1 + float64(int(datas[1]))/16)
+		log.Printf("> underruns=%-4v buflen=%-4v tickavg=%-12s drift=%-12s | bytes=%-4v freqs=%-9v\n",
+			al.Underruns(), al.BufLen(), al.TickAverge(), al.DriftApprox(), datas, freqs)
+		mod.SetFreq(freqs[0], nil)
+		osc.SetFreq(freqs[1], mod)
 	}
 }
